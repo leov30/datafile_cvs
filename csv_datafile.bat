0bb696bea7f93735e453c5a90a737af435a85150
leov30 <leo_v30@hotmail.com>
2022-11-20 12:04:20 -0800

    Add files via upload

--- csv_datafile.bat
+++ csv_datafile.bat
@@ -1,15 +1,15 @@
 @echo off
+set "_dat=%~1"
+if "%_dat%"=="" echo DRAG AND DROP DATAFILE TO THIS SCRIPT&pause&exit
 
-set "_dat="
-for %%g in (*.dat) do set "_dat=%%g"
-if "%_dat%"=="" echo NO DATAFILE WAS FOUND&pause&exit
-
+set "_output=%~n1"
+cd /d "%~dp0"
 title Processing...
 findstr /b /c:"		<rom name=" "%_dat%" >temp.txt
 
-(echo rom_name,size,crc,md5,sha1) >output.csv
+(echo rom_name,size,crc,md5,sha1) >"%_output%.csv"
 for /f tokens^=2^,4^,6^,8^,10^ delims^=^" %%g in (temp.txt) do (
-	(echo "%%g",%%h,%%i,%%j,%%k) >>output.csv
+	(echo "%%g",%%h,%%i,%%j,%%k) >>"%_output%.csv"
 )
 del temp.txt
 title FINISHED
